ebfdf846a92f1700c44388e74f0bec0d56dfa165
EALongva <e.a.longva@fys.uio.no>
2022-02-23 04:14:47 +0100

    setting up the project

---
+++ project1/setup.py
@@ -0,0 +1,31 @@
+# Initially interested in learning to use the machine learning resources provided in microsoft azure, but testing out ML algorithms locally for now
+# testing the tutorials found on https://machinelearningmastery.com/machine-learning-in-python-step-by-step/ by Jason Brownlee
+
+
+### this is actually quite a useful setup to validate package versions!
+# Check the versions of libraries
+ 
+# Python version
+import sys
+print('Python: {}'.format(sys.version))
+
+# scipy
+import scipy
+print('scipy: {}'.format(scipy.__version__))
+
+# numpy
+import numpy
+print('numpy: {}'.format(numpy.__version__))
+
+# matplotlib
+import matplotlib
+print('matplotlib: {}'.format(matplotlib.__version__))
+
+# pandas
+import pandas
+print('pandas: {}'.format(pandas.__version__))
+
+# scikit-learn
+import sklearn
+print('sklearn: {}'.format(sklearn.__version__))
+
